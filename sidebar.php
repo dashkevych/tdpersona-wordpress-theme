77d80951bd15371555906b857e14d25c2d062389
Taras Dashkevych <trdashix@gmail.com>
2014-12-14 22:59:28 -0800

    Fixed sidebar

--- sidebar.php
+++ sidebar.php
@@ -12,24 +12,6 @@
 
 <div id="secondary" class="widget-area" role="complementary">
 	<div id="sidebar-content" class="widget-inner">
-		<aside id="search" class="widget widget_search">
-			<?php get_search_form(); ?>
-		</aside><!-- .widget -->
-
-		<aside id="archives" class="widget">
-			<h4 class="widget-title"><?php _e( 'Archives', 'tdpersona' ); ?></h4>
-			<ul>
-				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-			</ul>
-		</aside><!-- .widget -->
-
-		<aside id="meta" class="widget">
-			<h4 class="widget-title"><?php _e( 'Meta', 'tdpersona' ); ?></h4>
-			<ul>
-				<?php wp_register(); ?>
-				<li><?php wp_loginout(); ?></li>
-				<?php wp_meta(); ?>
-			</ul>
-		</aside><!-- .widget -->
+		<?php dynamic_sidebar( 'sidebar-4' ); ?>
 	</div><!-- .widget-inner -->
 </div><!-- #secondary .widget-area -->
