b07db63b2196d285c8def780ee4b9a034dcdfed8
Taras Dashkevych <trdashix@gmail.com>
2014-12-14 22:43:42 -0800

    Fixed sidebar issues

--- sidebar.php
+++ sidebar.php
@@ -5,32 +5,31 @@
  * @package tdpersona
  * @since tdpersona 1.0
  */
+
+ if ( ! is_active_sidebar( 'sidebar-4' ) )
+	return;
 ?>
 
-		<div id="secondary" class="widget-area" role="complementary">			
-			<div id="sidebar-content" class="widget-inner">
-				<?php if ( ! dynamic_sidebar( 'sidebar-4' ) ) : ?>
-	
-					<aside id="search" class="widget widget_search">
-						<?php get_search_form(); ?>
-					</aside>
-	
-					<aside id="archives" class="widget">
-						<h1 class="widget-title"><?php _e( 'Archives', 'tdsimple' ); ?></h1>
-						<ul>
-							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-						</ul>
-					</aside>
-	
-					<aside id="meta" class="widget">
-						<h1 class="widget-title"><?php _e( 'Meta', 'tdsimple' ); ?></h1>
-						<ul>
-							<?php wp_register(); ?>
-							<li><?php wp_loginout(); ?></li>
-							<?php wp_meta(); ?>
-						</ul>
-					</aside>
-	
-				<?php endif; // end sidebar widget area ?>
-			</div>
-		</div><!-- #secondary .widget-area -->
+<div id="secondary" class="widget-area" role="complementary">
+	<div id="sidebar-content" class="widget-inner">
+		<aside id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</aside><!-- .widget -->
+
+		<aside id="archives" class="widget">
+			<h4 class="widget-title"><?php _e( 'Archives', 'tdpersona' ); ?></h4>
+			<ul>
+				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		</aside><!-- .widget -->
+
+		<aside id="meta" class="widget">
+			<h4 class="widget-title"><?php _e( 'Meta', 'tdpersona' ); ?></h4>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</aside><!-- .widget -->
+	</div><!-- .widget-inner -->
+</div><!-- #secondary .widget-area -->
