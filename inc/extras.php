ab7234ede976c1e0312b5221d98655eef47fa587
Taras Dashkevych <trdashix@gmail.com>
2016-09-14 14:01:54 -0700

    Removed unnecessary function;

--- inc/extras.php
+++ inc/extras.php
@@ -35,64 +35,6 @@ function tdpersona_body_classes( $classes ) {
 add_filter( 'body_class', 'tdpersona_body_classes' );
 
 /**
- * Filter in a link to a content ID attribute for the next/previous image links on image attachment pages
- *
- * @since tdpersona 1.0
- */
-function tdpersona_enhanced_image_navigation( $url, $id ) {
-	if ( ! is_attachment() && ! wp_attachment_is_image( $id ) )
-		return $url;
-
-	$image = get_post( $id );
-	if ( ! empty( $image->post_parent ) && $image->post_parent != $id )
-		$url .= '#main';
-
-	return $url;
-}
-add_filter( 'attachment_link', 'tdpersona_enhanced_image_navigation', 10, 2 );
-
-if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @param string $title Default title text for current view.
-	 * @param string $sep Optional separator.
-	 * @return string The filtered title.
-	 */
-	function tdpersona_wp_title( $title, $sep ) {
-		if ( is_feed() ) {
-			return $title;
-		}
-		global $page, $paged;
-		// Add the blog name
-		$title .= get_bloginfo( 'name', 'display' );
-		// Add the blog description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= " $sep $site_description";
-		}
-		// Add a page number if necessary:
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= " $sep " . sprintf( __( 'Page %s', '_s' ), max( $paged, $page ) );
-		}
-		return $title;
-	}
-	add_filter( 'wp_title', 'tdpersona_wp_title', 10, 2 );
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function tdpersona_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
-	}
-	add_action( 'wp_head', 'tdpersona_render_title' );
-endif;
-
-/**
  * Return logo style class
  */
 function tdpersona_get_logo_class() {
